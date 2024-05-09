1bf54618186c00be85d13b79b1bf60c53d478b78
dmoyan0 <diego.moyano@usm.cl>
2024-05-08 22:37:39 -0400

    Update Director.go

--- MV1/Director.go
+++ MV1/Director.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
 
+	pb "github.com/dmoyan0/Lab4";
+	"google.golang.org/grpc"
+)
+
+func main() {
+	dsaasd
 }
